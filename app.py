1517f1168d60930f4380b4e37be9449332437671
micymike <mosesmichael878@gmail.com>
2024-07-11 17:00:21 +0300

    "Reordered and removed 'app' parameter from socketio.run() call"

--- app.py
+++ app.py
@@ -219,4 +219,4 @@ def handle_disconnect():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
